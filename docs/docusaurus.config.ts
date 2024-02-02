4b4e03735ed204e9b56a8e396895908d4198fb84
hirohata <hirohatatro@gmail.com>
2024-02-02 11:56:55 -0800

    fix: base url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://dummy', // TODO: write the correct URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tascurator-documents/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
